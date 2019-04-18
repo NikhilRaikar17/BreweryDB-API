2f8b8afcdabfe12920a6c97a328e0396201287cd
Nikhil Raikar <nikhilraikar88@gmail.com>
2019-04-18 13:24:55 +0000

    Design Changes

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import $ from 'jquery';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Listing extends React.Component {
 
@@ -8,7 +10,7 @@ class Listing extends React.Component {
     }
 
   componentDidMount(){
-    this.getbeer();
+    this.getbeer();   
   }
 
   getbeer = () => {
@@ -17,9 +19,7 @@ class Listing extends React.Component {
       method: 'get',
       
     }).then(data => {
-      // console.log(data.data.data) 
       this.setState({data_beer: data.data.data})
-
       }
     )
 
@@ -29,23 +29,30 @@ class Listing extends React.Component {
     const data = this.state.data_beer;
 
     return(
-    <div class="container"> 
-    <table class="table table-hover">
+    <div className="container-fluid"> 
+    <div className="text-center" style={{}}>
+    <img className="img-responsive brewery-img" src={require('../img/B.png')} />
+    </div>
+    <div className="table-responsive">
+    <table className="table table-hover">
     <thead>
     <tr>
-      <th scope="col">Beer Name</th>
+      <th scope="col"><i className="fas fa-beer" style={{width:20}}></i>Beer Name</th>
       <th scope="col">Beer-abv</th>
       <th scope="col">Beer-ibu</th>
     </tr>
     </thead>
-    <tbody>
-              
+    <tbody>              
      {this.renderListing(data)}
-    
     </tbody>
     </table>
     </div>
-
+      <div className="container my-auto">
+        <div className="copyright text-center my-auto">
+          <span>Copyright © BreweryDB</span>
+        </div>
+      </div>   
+    </div>
     )
   }
 
@@ -53,7 +60,7 @@ class Listing extends React.Component {
       const link = `/details/${element.id}`
       return (
       <tr>
-        <th scope="row"><a href={link} target="_blank" style={{marginRight:5}}>{element.name}</a></th>
+        <th scope="row"><a className="nounderline" href={link} target="_blank" style={{marginRight:5}}>{element.name}</a></th>
         <td>{element.abv}</td>
         <td>{element.ibu}</td>
       </tr>               

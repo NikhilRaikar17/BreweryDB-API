96bd3f8f16ebdeb1e990ccb0961c9a7c76d8755d
Nikhil Raikar <nikhilraikar88@gmail.com>
2019-04-24 19:40:30 +0000

    Updated Components and design changes

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
-import axios from "axios";
+import './Details.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Details extends React.Component {  
 	state = {
@@ -15,24 +16,112 @@ class Details extends React.Component {
     fetch(`/details/${id}`)
     .then(res => res.json())
     .then(data => {
-      console.log(data.data)
+      // console.log(data.data.labels.icon)
       this.setState({data_beer_details: data.data})
+      // console.log(data_beer_details.labels.icon)
       }
     )
 
   }
   
 	render(){
-		const data = this.state.data_beer_details;
-		return(
-		<div>  
-	    <p>Name:{this.state.data_beer_details.name}</p>
-	    <p>Abv:{this.state.data_beer_details.abv}</p>
-	    <p>Ibu:{this.state.data_beer_details.ibu}</p>
-	    <p>Organic:{this.state.data_beer_details.isOrganic}</p>
-	    <p>Year:{this.state.data_beer_details.year}</p>
-	    <p>Status:{this.state.data_beer_details.status}</p>
-	    </div>
+		return(        
+        <div className="container-fluid"> 
+          <nav className="navbar navbar-expand navbar-dark mb-3 static-top" style={{backgroundColor: "rgb(255,250,250)"}}>
+          <img src="img-responsive brewery-img" src={require('../img/brewerydb.png')} style={{height: 50}} alt="BewerbyDB" />
+          </nav>
+          <div id="wrapper">    
+          <div id="content-wrapper">
+          <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="">Beer Listings</a>
+          </li>
+          <li className="breadcrumb-item">Details</li>
+        </ol>
+        </div>
+        </div>
+        <div>  
+        <div className ="page-heading-two"> 
+        {this.state.data_beer_details.name && <h1 className="name-heading"><FontAwesomeIcon icon="blender"/> {this.state.data_beer_details.name} </h1>}
+        </div>
+        <div className="table-responsive">
+        <table className="table">
+        <thead>
+        <tr>
+          <th scope="col">Beer Details </th>
+          <th scope="col">Beer Information</th>
+        </tr>
+        </thead>
+        <tbody> 
+        <tr>
+          <td className="name-heading"> Alcohol By Volume </td>
+          <td>         
+            {
+              this.state.data_beer_details.abv && 
+                <span>{this.state.data_beer_details.abv}</span>
+            }
+          </td>
+          </tr>
+          <tr>
+          <td className="name-heading"> International Bitterness Units </td>
+          <td>         
+            {
+              this.state.data_beer_details.ibu &&
+                <span>{this.state.data_beer_details.ibu}</span>
+            }
+          </td>
+          </tr>
+          <tr>
+          <td className="name-heading"> IsOrganic </td>
+          <td>    
+            {
+              this.state.data_beer_details.isOrganic && 
+                <span>{this.state.data_beer_details.isOrganic}</span>
+            }
+          </td>
+          </tr>
+          <tr>
+          <td className="name-heading"> Year </td>
+          <td>
+            {
+              this.state.data_beer_details.year && 
+                <span>{this.state.data_beer_details.year}</span>
+            }
+          </td>
+          </tr>
+          <tr>
+          <td className="name-heading"> Labels </td>
+          <td>
+            {
+              this.state.data_beer_details.labels && 
+                <span><img src={this.state.data_beer_details.labels.icon}/></span>
+            }
+          </td>
+          </tr>
+          <tr>
+          <td className="name-heading"> Status </td>
+          <td>
+            {
+              this.state.data_beer_details.status && 
+              <span>{this.state.data_beer_details.status}</span>
+            }
+          </td>
+          </tr>
+          <tr>
+          <td className="name-heading"> Glass Data </td>
+          <td>
+            {
+              this.state.data_beer_details.glass && 
+              <span>ID: {this.state.data_beer_details.id}, Name: {this.state.data_beer_details.name}, CreatedDate: {this.state.data_beer_details.createDate}</span>
+            }
+          </td>
+          </tr>
+        </tbody>
+        </table>
+        </div>
+        </div>
+        </div>
+       
 
 	  )
 	}

4e78533ff1e239d0b2bf3b82ac4b04e2189c45b1
Nikhil Raikar <nikhilraikar88@gmail.com>
2019-04-25 19:13:21 +0000

    Updated routes

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var API_Key = `36a9fd1572ee14caebcaf7834eda6905`
 
 /* GET home page. */
 router.get('/data', function(req, res, next) {
   request({
-  uri: 'https://sandbox-api.brewerydb.com/v2/beers/?key=36a9fd1572ee14caebcaf7834eda6905',
+  uri: `https://sandbox-api.brewerydb.com/v2/beers/?key=${API_Key}`,
 }).pipe(res);
 });
 
--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var API_Key = `36a9fd1572ee14caebcaf7834eda6905`
 
 /* GET home page. */
 router.get('/details/:id', function(req, res, next) {
   request({
-  uri: 'https://sandbox-api.brewerydb.com/v2/beer/'+req.params.id+'?key=36a9fd1572ee14caebcaf7834eda6905',
+  uri: `https://sandbox-api.brewerydb.com/v2/beer/`+req.params.id+`?key=${API_Key}`,
 }).pipe(res);
 });
 
